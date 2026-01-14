309ed7a4dcd1d48cddf9311a97eba530191a9eb0
johnrach <john.rachwalski1@gmail.com>
2026-01-13 21:39:09 -0500

    added better code

--- src/main/java/frc/robot/subsystems/FuelIntake.java
+++ src/main/java/frc/robot/subsystems/FuelIntake.java
@@ -1,255 +1,115 @@
-package frc.robot.subsystems.fuel_intake;
+package frc.robot.subsystems;
 
-import static edu.wpi.first.units.Units.Amps;
 import static edu.wpi.first.units.Units.Degrees;
 import static edu.wpi.first.units.Units.DegreesPerSecond;
 import static edu.wpi.first.units.Units.DegreesPerSecondPerSecond;
-import static edu.wpi.first.units.Units.Radians;
-import static edu.wpi.first.units.Units.Seconds;
+import static edu.wpi.first.units.Units.Feet;
+import static edu.wpi.first.units.Units.Pounds;
 import static edu.wpi.first.units.Units.Volts;
 
-import java.util.function.Supplier;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
 
 import edu.wpi.first.math.controller.ArmFeedforward;
-import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
-import edu.wpi.first.units.measure.Angle;
-import edu.wpi.first.units.measure.AngularVelocity;
-import edu.wpi.first.units.measure.Voltage;
-import edu.wpi.first.wpilibj.RobotState;
-import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
-import yams.mechanisms.SmartMechanism;
+import yams.mechanisms.config.ArmConfig;
+import yams.mechanisms.positional.Arm;
+import yams.motorcontrollers.SmartMotorController;
 import yams.motorcontrollers.SmartMotorControllerConfig;
 import yams.motorcontrollers.SmartMotorControllerConfig.ControlMode;
 import yams.motorcontrollers.SmartMotorControllerConfig.MotorMode;
 import yams.motorcontrollers.SmartMotorControllerConfig.TelemetryVerbosity;
+import yams.motorcontrollers.local.SparkWrapper;
 
-public class FuelIntakeSubsystem extends SubsystemBase {
+public class FuelIntake extends SubsystemBase {
+
+  private final SmartMotorController pivotMotor;
+  private final SmartMotorController rollerMotor;
+  private final Arm pivotArm;
 
   public enum Goal {
-    STOW(Degrees.of(0), Volts.of(0)),
-    INTAKING(Degrees.of(90), Volts.of(12)),
-    MANUAL();
+    //these could be the values i have truly no idea
+    STOW(0.0, 0.0),
+    INTAKING(90.0, 12.0);
 
-    private Angle angle;
-    private Voltage rollerVoltage;
+    private final double angleDeg;
+    private final double rollerVoltage;
 
-    private Goal(Angle angle, Voltage rollerVoltage) {
-      this.angle = angle;
+    Goal(double angleDeg, double rollerVoltage) {
+      this.angleDeg = angleDeg;
       this.rollerVoltage = rollerVoltage;
     }
 
-    private Goal() {
+    public double getAngleDeg() {
+      return angleDeg;
     }
 
-    public Angle getAngle() {
-      return this.angle;
+    public double getRollerVoltage() {
+      return rollerVoltage;
     }
-
-    public Voltage getRollerVoltage() {
-      return this.rollerVoltage;
-    }
-  }
-
-  private static final class Constants {
-    static final int PIVOT_MOTOR_ID = 10;
-    static final int ROLLER_MOTOR_ID = 11;
-
-    static final Angle MIN_ANGLE = Degrees.of(-5);
-    static final Angle MAX_ANGLE = Degrees.of(100);
-
-    static final double PIVOT_GEAR_RATIO = 80.0;
-
-    static final double PIVOT_KP = 50.0;
-    static final double PIVOT_KI = 0.0;
-    static final double PIVOT_KD = 0.0;
-    static final double PIVOT_MAX_VELOCITY = 180.0;
-    static final double PIVOT_MAX_ACCELERATION = 360.0;
-
-    static final double PIVOT_KS = 0.0;
-    static final double PIVOT_KG = 0.5;
-    static final double PIVOT_KV = 0.0;
-
-    static final double PIVOT_CURRENT_LIMIT = 40.0;
-    static final double ROLLER_CURRENT_LIMIT = 30.0;
-
-    static final Angle AT_GOAL_TOLERANCE = Degrees.of(2.0);
   }
 
   private Goal currentGoal = Goal.STOW;
 
-  public final Trigger atGoalTrigger = new Trigger(this::atGoal);
-
-  private SmartMechanism pivot;
-  private SmartMechanism roller;
-
-  private ArmFeedforward pivotFeedforward;
-  private ProfiledPIDController pidController;
-
-  private SmartMotorControllerConfig pivotConfig;
-  private SmartMotorControllerConfig rollerConfig;
-
-  public FuelIntakeSubsystem() {
-    pivotFeedforward = new ArmFeedforward(Constants.PIVOT_KS, Constants.PIVOT_KG, Constants.PIVOT_KV);
-
-    pidController = new ProfiledPIDController(
-        Constants.PIVOT_KP,
-        Constants.PIVOT_KI,
-        Constants.PIVOT_KD,
-        new Constraints(
-            Constants.PIVOT_MAX_VELOCITY, Constants.PIVOT_MAX_ACCELERATION));
-
-    pidController.setIntegratorRange(-0.2, 0.2);
-    pidController.setIZone(0.2);
-
-    pivotConfig = new SmartMotorControllerConfig(this)
+  public FuelIntake() {
+    SmartMotorControllerConfig pivotCfg = new SmartMotorControllerConfig(this)
         .withControlMode(ControlMode.CLOSED_LOOP)
-        .withClosedLoopController(
-            Constants.PIVOT_KP,
-            Constants.PIVOT_KI,
-            Constants.PIVOT_KD,
-            DegreesPerSecond.of(Constants.PIVOT_MAX_VELOCITY),
-            DegreesPerSecondPerSecond.of(Constants.PIVOT_MAX_ACCELERATION))
-        .withSimClosedLoopController(
-            Constants.PIVOT_KP,
-            Constants.PIVOT_KI,
-            Constants.PIVOT_KD,
-            DegreesPerSecond.of(Constants.PIVOT_MAX_VELOCITY),
-            DegreesPerSecondPerSecond.of(Constants.PIVOT_MAX_ACCELERATION))
-        .withFeedforward(pivotFeedforward)
-        .withSimFeedforward(pivotFeedforward)
-        .withTelemetry("FuelIntakePivot", TelemetryVerbosity.HIGH)
-        .withGearing(Constants.PIVOT_GEAR_RATIO)
+        .withClosedLoopController(0.6, 0.0, 0.05, DegreesPerSecond.of(180), DegreesPerSecondPerSecond.of(90))
+        .withFeedforward(new ArmFeedforward(0.1, 0.4, 0.01))
+        .withTelemetry("PivotMotor", TelemetryVerbosity.HIGH)
         .withMotorInverted(false)
-        .withIdleMode(MotorMode.BRAKE)
-        .withStatorCurrentLimit(Amps.of(Constants.PIVOT_CURRENT_LIMIT))
-        .withClosedLoopRampRate(Seconds.of(0.25))
-        .withOpenLoopRampRate(Seconds.of(0.25));
+        .withIdleMode(MotorMode.BRAKE);
 
-    rollerConfig = new SmartMotorControllerConfig(this)
+    SparkMax pivotSpark = new SparkMax(4, MotorType.kBrushless);
+    pivotMotor = new SparkWrapper(pivotSpark, DCMotor.getNEO(1), pivotCfg);
+
+    SmartMotorControllerConfig rollerCfg = new SmartMotorControllerConfig(this)
         .withControlMode(ControlMode.OPEN_LOOP)
-        .withTelemetry("FuelIntakeRoller", TelemetryVerbosity.MEDIUM)
-        .withMotorInverted(false)
         .withIdleMode(MotorMode.COAST)
-        .withStatorCurrentLimit(Amps.of(Constants.ROLLER_CURRENT_LIMIT))
-        .withOpenLoopRampRate(Seconds.of(0.1));
+        .withTelemetry("RollerMotor", TelemetryVerbosity.HIGH);
 
-    pivot = new SmartMechanism(Constants.PIVOT_MOTOR_ID, pivotConfig);
-    roller = new SmartMechanism(Constants.ROLLER_MOTOR_ID, rollerConfig);
+    SparkMax rollerSpark = new SparkMax(5, MotorType.kBrushless);
+    rollerMotor = new SparkWrapper(rollerSpark, DCMotor.getNEO(1), rollerCfg);
 
-    setGoal(Goal.STOW);
+    ArmConfig armCfg = new ArmConfig(pivotMotor)
+        .withSoftLimits(Degrees.of(-20), Degrees.of(150))
+        .withStartingPosition(Degrees.of(0))
+        .withLength(Feet.of(1.5))
+        .withMass(Pounds.of(4.0))
+        .withTelemetry("PivotArm", TelemetryVerbosity.HIGH);
+
+    pivotArm = new Arm(armCfg);
   }
 
   @Override
   public void periodic() {
-    double timestamp = Timer.getFPGATimestamp();
+    pivotArm.updateTelemetry();
+    //move the arm to where we want it to be using currentgoal angle i have no idea what this is right now
+    pivotArm.setAngle(Degrees.of(currentGoal.getAngleDeg()));
 
-    if (RobotState.isDisabled()) {
-      double position = getPosition().in(Radians);
-      pidController.setGoal(position);
-      pidController.reset(position);
-    }
+    rollerMotor.setVoltage(Volts.of(currentGoal.getRollerVoltage()));
+  }
 
-    Angle desiredAngle = currentGoal.getAngle();
-    Voltage desiredRollerVoltage = currentGoal.getRollerVoltage();
-
-    Angle constrainedAngle = clampAngle(desiredAngle);
-
-    switch (currentGoal) {
-      case STOW:
-      case INTAKING:
-      case OUTTAKE:
-      case HANDOFF:
-        pivot.setReference(
-            Degrees.of(calculateVoltageForPosition(constrainedAngle).in(Volts)));
-        roller.setReference(desiredRollerVoltage.in(Volts) / 12.0);
-        break;
-      case MANUAL:
-        break;
-    }
+  @Override
+  public void simulationPeriodic() {
+    pivotArm.simIterate();
   }
 
   public void setGoal(Goal goal) {
     currentGoal = goal;
   }
 
-  public Goal getCurrentGoal() {
-    return currentGoal;
+  public Command stowCommand() {
+    return runOnce(() -> setGoal(Goal.STOW));
   }
 
-  public Angle getPosition() {
-    return Degrees.of(pivot.getPosition().in(Degrees));
-  }
-
-  public AngularVelocity getVelocity() {
-    return pivot.getVelocity();
-  }
-
-  private Angle clampAngle(Angle angle) {
-    if (angle.lt(Constants.MIN_ANGLE)) {
-      return Constants.MIN_ANGLE;
-    } else if (angle.gt(Constants.MAX_ANGLE)) {
-      return Constants.MAX_ANGLE;
-    }
-    return angle;
-  }
-
-  private Voltage calculateVoltageForPosition(Angle desired) {
-    Voltage pidVoltage = Volts.of(
-        pidController.calculate(
-            getPosition().in(Radians), desired.in(Radians)));
-
-    TrapezoidProfile.State setpoint = pidController.getSetpoint();
-    Voltage ffVoltage = Volts.of(pivotFeedforward.calculate(setpoint.position, setpoint.velocity));
-
-    return pidVoltage.plus(ffVoltage);
+  public Command intakeCommand() {
+    return runOnce(() -> setGoal(Goal.INTAKING));
   }
 
   public boolean atGoal() {
-    return atGoal(currentGoal);
-  }
-
-  public boolean atGoal(Goal goal) {
-    return atGoal(goal, Constants.AT_GOAL_TOLERANCE);
-  }
-
-  public boolean atGoal(Goal goal, Angle angleTolerance) {
-    return currentGoal == goal && getPosition().isNear(goal.getAngle(), angleTolerance);
-  }
-
-  public Trigger atGoalTrigger(Goal goal) {
-    return new Trigger(() -> atGoal(goal));
-  }
-
-  public Trigger atGoalTrigger(Goal goal, Angle angleTolerance) {
-    return new Trigger(() -> atGoal(goal, angleTolerance));
-  }
-
-  public Supplier<Command> stowCommand() {
-    return () -> setGoalCommand(Goal.STOW);
-  }
-
-  public Supplier<Command> intakeCommand() {
-    return () -> setGoalEndCommand(Goal.INTAKING, Goal.STOW);
-  }
-
-  public Command setGoalCommand(Goal goal) {
-    return runOnce(() -> setGoal(goal));
-  }
-
-  public Command setGoalEndCommand(Goal goal, Goal endGoal) {
-    return run(() -> setGoal(goal)).finallyDo(() -> setGoal(endGoal));
-  }
-
-  public Command setGoalAndWait(Goal goal) {
-    return run(() -> setGoal(goal)).until(this::atGoal);
-  }
-
-  public Command setGoalAndWait(Goal goal, Angle tolerance) {
-    return run(() -> setGoal(goal)).until(() -> atGoal(goal, tolerance));
+    return Math.abs(pivotArm.getAngle().in(Degrees) - currentGoal.getAngleDeg()) < 2.0;
   }
 }
