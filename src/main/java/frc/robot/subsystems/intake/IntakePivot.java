bbf998b9b79ea6efb5d1b1a17c87aa3aa9e5469d
Arun Palaniappan <apalaniappan@anduril.com>
2026-02-04 20:43:22 -0500

    ADding sim closed loop controller

--- src/main/java/frc/robot/subsystems/intake/IntakePivot.java
+++ src/main/java/frc/robot/subsystems/intake/IntakePivot.java
@@ -39,6 +39,7 @@ public class IntakePivot extends SubsystemBase {
     SmartMotorControllerConfig pivotCfg = new SmartMotorControllerConfig(this)
         .withControlMode(ControlMode.CLOSED_LOOP)
         .withClosedLoopController(0.6, 0.0, 0.05, DegreesPerSecond.of(180), DegreesPerSecondPerSecond.of(90))
+        .withSimClosedLoopController(3.0, 0.0, 0.05, DegreesPerSecond.of(180), DegreesPerSecondPerSecond.of(90))
         .withFeedforward(new ArmFeedforward(0.1, 0.4, 0.01))
         .withTelemetry("PivotMotor", TelemetryVerbosity.HIGH)
         .withMotorInverted(false)
