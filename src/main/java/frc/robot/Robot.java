4fe6b934926c914d943a06deb1ca6abcd96bd846
woodward spivey <woodyspivey@gmail.com>
2026-02-09 17:17:52 -0500

    i added some stuff ig idk

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,9 +1,5 @@
 package frc.robot;
 
-import static edu.wpi.first.units.Units.Radians;
-
-import java.util.function.Supplier;
-
 import choreo.auto.AutoChooser;
 import choreo.auto.AutoFactory;
 import dev.doglog.DogLog;
@@ -11,8 +7,6 @@ import dev.doglog.DogLogOptions;
 import edu.wpi.first.epilogue.Epilogue;
 import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.math.geometry.Transform3d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.PowerDistribution;
@@ -94,14 +88,7 @@ public class Robot extends TimedRobot {
     m_autoRoutines = new AutoRoutines(m_autoFactory);
     m_autoChooser = new AutoChooser("Do Nothing");
 
-    final Supplier<Angle> turretAngleToHub = () -> {
-      Translation2d hubPosition = GeometryUtil.flip(FieldConstants.kHubPosition.toTranslation2d());
-      final Translation2d diff = m_state.getRobotPose().getTranslation().minus(hubPosition);
-
-      return diff.getAngle().minus(m_state.getRobotPose().getRotation()).getMeasure().plus(Radians.of(Math.PI));
-    };
-
-    m_turret.setDefaultCommand(m_turret.setAngleCommand(turretAngleToHub));
+    m_turret.setDefaultCommand(m_state.turretTrack());
     generateAutoChooser();
   }
 
--- src/main/java/frc/robot/RobotState.java
+++ src/main/java/frc/robot/RobotState.java
@@ -1,15 +1,21 @@
 package frc.robot;
 
 import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.Radians;
 import static edu.wpi.first.units.Units.RadiansPerSecond;
+
+import java.util.function.Supplier;
 
 import com.ctre.phoenix6.swerve.SwerveRequest;
 
 import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.LinearVelocity;
+import edu.wpi.first.units.measure.Time;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -25,6 +31,8 @@ public class RobotState {
   public final CommandSwerveDrivetrain m_drive;
   public final IntakePivot m_intakePivot;
   public final Turret m_turret;
+
+  private final InterpolatingDoubleTreeMap m_timeOfFlightMap = new InterpolatingDoubleTreeMap();
 
   public RobotState(Controls controls, CommandSwerveDrivetrain drive, IntakePivot intakePivot, Turret turret) {
     m_controls = controls;
@@ -85,6 +93,10 @@ public class RobotState {
     return m_turret.getAngle();
   }
 
+  public Time getTimeOfFlight(Translation2d target) {
+
+  }
+
   public Trigger inAllianceZone() {
     return new Trigger(
         () -> DriverStation.getAlliance()
@@ -92,5 +104,16 @@ public class RobotState {
             .map(r -> r.contains(m_drive.getPose().getTranslation()))
             .orElse(false))
         .debounce(0.2);
+  }
+
+  public Command turretTrack() {
+    final Supplier<Angle> turretAngleToHub = () -> {
+      Translation2d hubPosition = GeometryUtil.flip(FieldConstants.kHubPosition.toTranslation2d());
+      final Translation2d diff = getRobotPose().getTranslation().minus(hubPosition);
+
+      return diff.getAngle().minus(getRobotPose().getRotation()).getMeasure().plus(Radians.of(Math.PI));
+    };
+
+    return m_turret.setAngleCommand(turretAngleToHub);
   }
 }
