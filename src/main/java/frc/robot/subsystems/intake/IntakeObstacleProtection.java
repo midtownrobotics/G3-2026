e9c8f08fe634d9ceddd0134740895541bfac3252
jrrachat <john.rachwalski1@gmail.com>
2026-02-23 18:22:47 -0500

    fixed changes based on thread

--- src/main/java/frc/robot/subsystems/intake/IntakeObstacleProtection.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeObstacleProtection.java
@@ -9,9 +9,12 @@ import java.nio.file.Files;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.Vector;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.numbers.N2;
 import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.units.measure.Time;
 import edu.wpi.first.wpilibj.Filesystem;
@@ -21,6 +24,11 @@ import frc.robot.RobotState;
 public class IntakeObstacleProtection {
   private static final Time kTimeThreshold = Seconds.of(1.5);
   private static final Distance kMaxScanDistance = Meters.of(3.0);
+
+  private static final double kMinApproachSpeed = 2;
+
+  private static final double kIntakeOffsetX = 0.59182;
+  private static final double kIntakeHalfWidth = 0.397129;
 
   private IntakeObstacleProtection() {
   }
@@ -46,34 +54,65 @@ public class IntakeObstacleProtection {
 
     double totalSpeed = Math.hypot(speeds.vxMetersPerSecond, speeds.vyMetersPerSecond);
 
+    if (totalSpeed <= kMinApproachSpeed) {
+      return false;
+    }
+
     if (totalSpeed <= 0) {
       return false;
     }
 
     Rotation2d heading = robotState.getRobotPose().getRotation();
-    double directionX = heading.getCos();
-    double directionY = heading.getSin();
+    double directionX = speeds.vxMetersPerSecond;
+    double directionY = speeds.vyMetersPerSecond;
+
+    Vector<N2> intakeDirection = VecBuilder.fill(heading.getCos(), heading.getSin());
+    Vector<N2> robotVelocity = VecBuilder.fill(directionX, directionY);
+
+    if (intakeDirection.dot(robotVelocity) <= 0) {
+      return false;
+    }
+
+    Translation2d intakeForward = new Translation2d(1, 0).rotateBy(heading);
+    Translation2d velocity = new Translation2d(directionX, directionY);
+
+    if (velocity.getX() * intakeForward.getX() + velocity.getY() * intakeForward.getY() <= 0) {
+      return false;
+    }
 
     Translation2d robotPosition = robotState.getRobotPose().getTranslation();
-    double distanceToObstacle = distanceToNearestObstacle(robotPosition, directionX, directionY, grid,
-        nodeSize);
 
-    if (distanceToObstacle == -1) {
+    Translation2d leftCorner = robotPosition.plus(
+        new Translation2d(kIntakeOffsetX, kIntakeHalfWidth).rotateBy(heading));
+    Translation2d rightCorner = robotPosition.plus(
+        new Translation2d(kIntakeOffsetX, -kIntakeHalfWidth).rotateBy(heading));
+
+    double leftDist = distanceToNearestObstacle(leftCorner, directionX, directionY, grid, nodeSize);
+    double rightDist = distanceToNearestObstacle(rightCorner, directionX, directionY, grid, nodeSize);
+
+    double distanceToObstacle;
+    if (leftDist == -1 && rightDist == -1) {
       return false;
+    } else if (leftDist == -1) {
+      distanceToObstacle = rightDist;
+    } else if (rightDist == -1) {
+      distanceToObstacle = leftDist;
+    } else {
+      distanceToObstacle = Math.min(leftDist, rightDist);
     }
 
     return distanceToObstacle / totalSpeed < kTimeThreshold.in(Seconds);
   }
 
   private static double distanceToNearestObstacle(
-      Translation2d robotPosition, double velocityDirectionX, double velocityDirectionY, boolean[][] grid,
+      Translation2d origin, double velocityDirectionX, double velocityDirectionY, boolean[][] grid,
       double nodeSize) {
 
     double maxDist = kMaxScanDistance.in(Meters);
 
     for (double distance = 0; distance <= maxDist; distance += nodeSize) {
-      int col = (int) ((robotPosition.getX() + velocityDirectionX * distance) / nodeSize);
-      int row = (int) ((robotPosition.getY() + velocityDirectionY * distance) / nodeSize);
+      int col = (int) ((origin.getX() + velocityDirectionX * distance) / nodeSize);
+      int row = (int) ((origin.getY() + velocityDirectionY * distance) / nodeSize);
 
       if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length) {
         return -1;
