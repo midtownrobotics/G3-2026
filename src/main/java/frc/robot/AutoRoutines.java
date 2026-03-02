9fbab0eadda725d4189f64005fa961a18f99ce4d
G3-Robotics <students@g3robotics.com>
2026-03-02 18:32:43 -0500

    add a few paths

--- src/main/java/frc/robot/AutoRoutines.java
+++ src/main/java/frc/robot/AutoRoutines.java
@@ -3,7 +3,9 @@ package frc.robot;
 import choreo.auto.AutoFactory;
 import choreo.auto.AutoRoutine;
 import choreo.auto.AutoTrajectory;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.CommandSwerveDrivetrain;
 
 public class AutoRoutines {
   private final AutoFactory m_autoFactory;
@@ -12,16 +14,27 @@ public class AutoRoutines {
     m_autoFactory = autoFactory;
   }
 
-  public AutoRoutine exampleMovementAuto() {
-    AutoRoutine routine = m_autoFactory.newRoutine("exampleMovementRoutine");
-
-    AutoTrajectory driveToMiddle = routine.trajectory("ExampleMovement");
-
+  public AutoRoutine depotToMidLeft() {
+    AutoRoutine routine = m_autoFactory.newRoutine("DepotToMidLeft");
+    AutoTrajectory depotToMidLeft = routine.trajectory("DepotToMidLeft");
+    
     routine.active().onTrue(
-        Commands.sequence(
-            driveToMiddle.resetOdometry(),
-            driveToMiddle.cmd()));
-
+      Commands.sequence(
+        depotToMidLeft.resetOdometry(),
+        depotToMidLeft.cmd()));
     return routine;
   }
+
+  public AutoRoutine leftStartToCenter() {
+    AutoRoutine routine = m_autoFactory.newRoutine("leftStartToCenter");
+    AutoTrajectory leftStartToCenter = routine.trajectory("leftStartToCenter");
+    
+    routine.active().onTrue(
+      Commands.sequence(
+        leftStartToCenter.resetOdometry(),
+        leftStartToCenter.cmd()));
+    return routine;
+  }
+
 }
+
