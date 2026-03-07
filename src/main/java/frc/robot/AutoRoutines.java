20aec00e38d399ca2974c41451518df18e7af65f
jrrachat <john.rachwalski1@gmail.com>
2026-03-07 09:33:51 -0500

    consolidate paths for easier usage

--- src/main/java/frc/robot/AutoRoutines.java
+++ src/main/java/frc/robot/AutoRoutines.java
@@ -22,248 +22,161 @@ public class AutoRoutines {
     trajectory.active().onTrue(m_commands.revShooter());
     trajectory.doneDelayed(0.5).onTrue(m_commands.autoAimAndPrepareShootAutonomous());
   }
-
-  public AutoRoutine depotToLeftStart() {
-    AutoRoutine routine = m_autoFactory.newRoutine("DepotToLeftStart");
-    AutoTrajectory depotToLeftStart = routine.trajectory("DepotToLeftStart");
-    
-    autoRoutineShoot(depotToLeftStart);
-
-    routine.active().onTrue(
-      Commands.sequence(
-        Commands.runOnce(() -> {
-          depotToLeftStart.getInitialPose().ifPresent(pose -> m_logger.log("InitialPose", pose));
-          m_logger.log("ActiveTrajectory", "DepotToLeftStart");
-        }),
-        depotToLeftStart.resetOdometry(),
-        depotToLeftStart.cmd()));
-    return routine;
-  }
-
-  public AutoRoutine depotToMidLeft() {
-    AutoRoutine routine = m_autoFactory.newRoutine("DepotToMidLeft");
+  
+  public AutoRoutine leftStartDepotPath() {
+    AutoRoutine routine = m_autoFactory.newRoutine("LeftStartToDepotPath");
+    AutoTrajectory leftStartToDepot = routine.trajectory("LeftStartToDepot");
     AutoTrajectory depotToMidLeft = routine.trajectory("DepotToMidLeft");
 
-    autoRoutineShoot(depotToMidLeft);
+    leftStartToDepot.atPose("intaking begins", 0.3, 0.4).onTrue(m_commands.runIntake());
+    leftStartToDepot.atPose("intaking ends", 0.3, 0.4).onTrue(m_commands.stowIntake());
 
+    depotToMidLeft.doneDelayed(0.5).onTrue(m_commands.autoAimAndPrepareShootAutonomous());
     routine.active().onTrue(
-      Commands.sequence(
-        Commands.runOnce(() -> {
-          depotToMidLeft.getInitialPose().ifPresent(pose -> m_logger.log("InitialPose", pose));
-          m_logger.log("ActiveTrajectory", "DepotToMidLeft");
-        }),
-        depotToMidLeft.resetOdometry(),
-        depotToMidLeft.cmd()));
+        Commands.sequence(
+            Commands.runOnce(() -> {
+              leftStartToDepot.getInitialPose().ifPresent(pose -> m_logger.log("InitialPose", pose));
+              m_logger.log("ActiveTrajectory", "leftStartToDepot");
+            }),
+            leftStartToDepot.resetOdometry(),
+            leftStartToDepot.cmd(),
+            Commands.runOnce(() -> {
+              depotToMidLeft.getInitialPose().ifPresent(pose -> m_logger.log("InitialPose", pose));
+              m_logger.log("ActiveTrajectory", "depotToMidLeft");
+            }),
+            depotToMidLeft.cmd()));
+
     return routine;
   }
 
-  public AutoRoutine leftStartToCenter() {
+  public AutoRoutine leftStartCenterPath() {
     AutoRoutine routine = m_autoFactory.newRoutine("LeftStartToCenter");
     AutoTrajectory leftStartToCenter = routine.trajectory("LeftStartToCenter");
 
     leftStartToCenter.atPose("intaking begins", 0.3, 0.4).onTrue(m_commands.runIntake());
     leftStartToCenter.atPose("intaking ends", 0.3, 0.4).onTrue(m_commands.stowIntake());
-    leftStartToCenter.atPose("shooting begins", 0.3, 0.4).onTrue(
-        Commands.sequence(
-          m_commands.revShooter(),
-          m_commands.autoAimAndPrepareShootAutonomous()  
-        )
-    );
-
-    routine.active().onTrue(
-      Commands.sequence(
-        Commands.runOnce(() -> {
-          leftStartToCenter.getInitialPose().ifPresent(pose -> m_logger.log("InitialPose", pose));
-          m_logger.log("ActiveTrajectory", "LeftStartToCenter");
-        }),
-        leftStartToCenter.resetOdometry(),
-        leftStartToCenter.cmd()));
-    return routine;
-  }
-
-  public AutoRoutine leftStartToDepot() {
-    AutoRoutine routine = m_autoFactory.newRoutine("LeftStartToDepot");
-    AutoTrajectory leftStartToDepot = routine.trajectory("LeftStartToDepot");
-
-    leftStartToDepot.atPose("intaking begins", 0.3, 0.4).onTrue(m_commands.runIntake());
-    leftStartToDepot.atPose("intaking ends", 0.3, 0.4).onTrue(m_commands.stowIntake());
-    
-    routine.active().onTrue(
-      Commands.sequence(
-        Commands.runOnce(() -> {
-          leftStartToDepot.getInitialPose().ifPresent(pose -> m_logger.log("InitialPose", pose));
-          m_logger.log("ActiveTrajectory", "LeftStartToDepot");
-        }),
-        leftStartToDepot.resetOdometry(),
-        leftStartToDepot.cmd()));
-    return routine;
-  }
-
-  public AutoRoutine midLeftToDepot() {
-    AutoRoutine routine = m_autoFactory.newRoutine("MidLeftToDepot");
-    AutoTrajectory midLeftToDepot = routine.trajectory("MidLeftToDepot");
-
-    midLeftToDepot.atPose("intaking begins", 0.3, 0.4).onTrue(m_commands.runIntake());
-    midLeftToDepot.atPose("intaking ends", 0.3, 0.4).onTrue(m_commands.stowIntake());
-
-    routine.active().onTrue(
-      Commands.sequence(
-        Commands.runOnce(() -> {
-          midLeftToDepot.getInitialPose().ifPresent(pose -> m_logger.log("InitialPose", pose));
-          m_logger.log("ActiveTrajectory", "MidLeftToDepot");
-        }),
-        midLeftToDepot.resetOdometry(),
-        midLeftToDepot.cmd()));
-    return routine;
-  }
-
- public AutoRoutine midRightToOutpost() {
-    AutoRoutine routine = m_autoFactory.newRoutine("MidRightToOutpost");
-    AutoTrajectory midRightToOutpost = routine.trajectory("MidRightToOutpost");
+    leftStartToCenter.doneDelayed(0.5).onTrue(m_commands.autoAimAndPrepareShootAutonomous());
 
     routine.active().onTrue(
         Commands.sequence(
             Commands.runOnce(() -> {
-                midRightToOutpost.getInitialPose().ifPresent(pose -> m_logger.log("InitialPose", pose));
-                m_logger.log("ActiveTrajectory", "MidRightToOutpost");
+              leftStartToCenter.getInitialPose().ifPresent(pose -> m_logger.log("InitialPose", pose));
+              m_logger.log("ActiveTrajectory", "LeftStartToCenter");
             }),
-            midRightToOutpost.resetOdometry(),
-            midRightToOutpost.cmd()));
+            leftStartToCenter.resetOdometry(),
+            leftStartToCenter.cmd()));
     return routine;
-}
-
-  public AutoRoutine midStartToDepot() {
-      AutoRoutine routine = m_autoFactory.newRoutine("MidStartToDepot");
-      AutoTrajectory midStartToDepot = routine.trajectory("MidStartToDepot");
-
-      midStartToDepot.atPose("intaking begins", 0.3, 0.4).onTrue(m_commands.runIntake());
-      midStartToDepot.atPose("intaking ends", 0.3, 0.4).onTrue(m_commands.stowIntake());
-
-
-      routine.active().onTrue(
-          Commands.sequence(
-              Commands.runOnce(() -> {
-                  midStartToDepot.getInitialPose().ifPresent(pose -> m_logger.log("InitialPose", pose));
-                  m_logger.log("ActiveTrajectory", "MidStartToDepot");
-              }),
-              midStartToDepot.resetOdometry(),
-              midStartToDepot.cmd()));
-      return routine;
   }
 
-  public AutoRoutine midStartToLeftStart() {
-      AutoRoutine routine = m_autoFactory.newRoutine("MidStartToLeftStart");
-      AutoTrajectory midStartToLeftStart = routine.trajectory("MidStartToLeftStart");
+  public AutoRoutine leftStartStealBallsPath() {
+    AutoRoutine routine = m_autoFactory.newRoutine("LeftToStealBalls");
+    AutoTrajectory leftToStealBalls = routine.trajectory("LeftToStealBalls");
 
-      routine.active().onTrue(
-          Commands.sequence(
-              Commands.runOnce(() -> {
-                  midStartToLeftStart.getInitialPose().ifPresent(pose -> m_logger.log("InitialPose", pose));
-                  m_logger.log("ActiveTrajectory", "MidStartToLeftStart");
-              }),
-              midStartToLeftStart.resetOdometry(),
-              midStartToLeftStart.cmd()));
-      return routine;
+    leftToStealBalls.atPose("intaking begins", 0.3, 0.4).onTrue(m_commands.runIntake());
+    leftToStealBalls.atPose("intaking ends", 0.3, 0.4).onTrue(m_commands.stowIntake());
+    leftToStealBalls.doneDelayed(0.5).onTrue(m_commands.autoAimAndPrepareShootAutonomous());
+
+    routine.active().onTrue(
+        Commands.sequence(
+            Commands.runOnce(() -> {
+              leftToStealBalls.getInitialPose().ifPresent(pose -> m_logger.log("InitialPose", pose));
+              m_logger.log("ActiveTrajectory", "LeftToStealBalls");
+            }),
+            leftToStealBalls.resetOdometry(),
+            leftToStealBalls.cmd()));
+    return routine;
   }
 
-  public AutoRoutine outpostToMidRight() {
-      AutoRoutine routine = m_autoFactory.newRoutine("OutpostToMidRight");
-      AutoTrajectory outpostToMidRight = routine.trajectory("OutpostToMidRight");
+  public AutoRoutine midStartCenterPath() {
+    AutoRoutine routine = m_autoFactory.newRoutine("MidStartCenterPath");
+    AutoTrajectory midStartToLeftstart = routine.trajectory("MidStartToLeftStart");
+    AutoTrajectory leftStartToCenter = routine.trajectory("LeftStartToCenter");
 
-      routine.active().onTrue(
-          Commands.sequence(
-              Commands.runOnce(() -> {
-                  outpostToMidRight.getInitialPose().ifPresent(pose -> m_logger.log("InitialPose", pose));
-                  m_logger.log("ActiveTrajectory", "OutpostToMidRight");
-              }),
-              outpostToMidRight.resetOdometry(),
-              outpostToMidRight.cmd()));
-      return routine;
+    leftStartToCenter.atPose("intaking begins", 0.3, 0.4).onTrue(m_commands.runIntake());
+    leftStartToCenter.atPose("intaking ends", 0.3, 0.4).onTrue(m_commands.stowIntake());
+    leftStartToCenter.doneDelayed(0.5).onTrue(m_commands.autoAimAndPrepareShootAutonomous());
+
+    routine.active().onTrue(
+        Commands.sequence(
+            Commands.runOnce(() -> {
+              midStartToLeftstart.getInitialPose().ifPresent(pose -> m_logger.log("InitialPose", pose));
+              m_logger.log("ActiveTrajectory", "midStartToLeftstart");
+            }),
+            midStartToLeftstart.resetOdometry(),
+            midStartToLeftstart.cmd(),
+            Commands.runOnce(() -> {
+              leftStartToCenter.getInitialPose().ifPresent(pose -> m_logger.log("InitialPose", pose));
+              m_logger.log("ActiveTrajectory", "leftStartToCenter");
+            }),
+            leftStartToCenter.cmd()));
+
+    return routine;
   }
 
-  public AutoRoutine rightStartToCenter() {
-      AutoRoutine routine = m_autoFactory.newRoutine("RightStartToCenter");
-      AutoTrajectory rightStartToCenter = routine.trajectory("RightStartToCenter");
-      
-      rightStartToCenter.atPose("intaking begins", 0.3, 0.4).onTrue(m_commands.runIntake());
-      rightStartToCenter.atPose("intaking ends", 0.3, 0.4).onTrue(m_commands.stowIntake());
+  public AutoRoutine midStartDepotPath() {
+    AutoRoutine routine = m_autoFactory.newRoutine("MidStartDepotPath");
+    AutoTrajectory midStartToDepot = routine.trajectory("MidStartToDepot");
+    AutoTrajectory depotToMidLeft = routine.trajectory("DepotToMidLeft");
 
-      routine.active().onTrue(
-          Commands.sequence(
-              Commands.runOnce(() -> {
-                  rightStartToCenter.getInitialPose().ifPresent(pose -> m_logger.log("InitialPose", pose));
-                  m_logger.log("ActiveTrajectory", "RightStartToCenter");
-              }),
-              rightStartToCenter.resetOdometry(),
-              rightStartToCenter.cmd()));
-      return routine;
+    midStartToDepot.atPose("intaking begins", 0.3, 0.4).onTrue(m_commands.runIntake());
+    midStartToDepot.atPose("intaking ends", 0.3, 0.4).onTrue(m_commands.stowIntake());
+    depotToMidLeft.doneDelayed(0.5).onTrue(m_commands.autoAimAndPrepareShootAutonomous());
+
+    routine.active().onTrue(
+        Commands.sequence(
+            Commands.runOnce(() -> {
+              midStartToDepot.getInitialPose().ifPresent(pose -> m_logger.log("InitialPose", pose));
+              m_logger.log("ActiveTrajectory", "midStartToDepot");
+            }),
+            midStartToDepot.resetOdometry(),
+            midStartToDepot.cmd(),
+            Commands.runOnce(() -> {
+              depotToMidLeft.getInitialPose().ifPresent(pose -> m_logger.log("InitialPose", pose));
+              m_logger.log("ActiveTrajectory", "depotToMidLeft");
+            }),
+            depotToMidLeft.cmd()));
+
+    return routine;
   }
 
-  public AutoRoutine rightToStealBalls() {
-      AutoRoutine routine = m_autoFactory.newRoutine("RightToStealBalls");
-      AutoTrajectory rightToStealBalls = routine.trajectory("RightToStealBalls");
+  public AutoRoutine rightStartCenterPath() {
+    AutoRoutine routine = m_autoFactory.newRoutine("RightStartCenterPath");
+    AutoTrajectory rightStartToCenter = routine.trajectory("RightStartToCenter");
 
-      rightToStealBalls.atPose("intaking begins", 0.3, 0.4).onTrue(m_commands.runIntake());
-      rightToStealBalls.atPose("intaking ends", 0.3, 0.4).onTrue(m_commands.stowIntake());
+    rightStartToCenter.atPose("intaking begins", 0.3, 0.4).onTrue(m_commands.runIntake());
+    rightStartToCenter.atPose("intaking ends", 0.3, 0.4).onTrue(m_commands.stowIntake());
+    rightStartToCenter.doneDelayed(0.5).onTrue(m_commands.autoAimAndPrepareShootAutonomous());
 
-
-      routine.active().onTrue(
-          Commands.sequence(
-              Commands.runOnce(() -> {
-                  rightToStealBalls.getInitialPose().ifPresent(pose -> m_logger.log("InitialPose", pose));
-                  m_logger.log("ActiveTrajectory", "RightToStealBalls");
-              }),
-              rightToStealBalls.resetOdometry(),
-              rightToStealBalls.cmd()));
-      return routine;
+    routine.active().onTrue(
+        Commands.sequence(
+            Commands.runOnce(() -> {
+              rightStartToCenter.getInitialPose().ifPresent(pose -> m_logger.log("InitialPose", pose));
+              m_logger.log("ActiveTrajectory", "LeftStartToCenter");
+            }),
+            rightStartToCenter.resetOdometry(),
+            rightStartToCenter.cmd()));
+    return routine;
   }
 
-  public AutoRoutine leftToStealBalls() {
-      AutoRoutine routine = m_autoFactory.newRoutine("LeftToStealBalls");
-      AutoTrajectory leftToStealBalls = routine.trajectory("LeftToStealBalls");
+  public AutoRoutine rightStartStealBallsPath() {
+    AutoRoutine routine = m_autoFactory.newRoutine("RightToStealBalls");
+    AutoTrajectory rightToStealBalls = routine.trajectory("RightToStealBalls");
 
-      leftToStealBalls.atPose("intaking begins", 0.3, 0.4).onTrue(m_commands.runIntake());
-      leftToStealBalls.atPose("intaking ends", 0.3, 0.4).onTrue(m_commands.stowIntake());
+    rightToStealBalls.atPose("intaking begins", 0.3, 0.4).onTrue(m_commands.runIntake());
+    rightToStealBalls.atPose("intaking ends", 0.3, 0.4).onTrue(m_commands.stowIntake());
+    rightToStealBalls.doneDelayed(0.5).onTrue(m_commands.autoAimAndPrepareShootAutonomous());
 
-
-      routine.active().onTrue(
-          Commands.sequence(
-              Commands.runOnce(() -> {
-                  leftToStealBalls.getInitialPose().ifPresent(pose -> m_logger.log("InitialPose", pose));
-                  m_logger.log("ActiveTrajectory", "LeftToStealBalls");
-              }),
-              leftToStealBalls.resetOdometry(),
-              leftToStealBalls.cmd()));
-      return routine;
+    routine.active().onTrue(
+        Commands.sequence(
+            Commands.runOnce(() -> {
+              rightToStealBalls.getInitialPose().ifPresent(pose -> m_logger.log("InitialPose", pose));
+              m_logger.log("ActiveTrajectory", "LeftToStealBalls");
+            }),
+            rightToStealBalls.resetOdometry(),
+            rightToStealBalls.cmd()));
+    return routine;
   }
 
-  public AutoRoutine leftStartToRightStart() {
-      AutoRoutine routine = m_autoFactory.newRoutine("LeftStartToRightStart");
-      AutoTrajectory leftStartToRightStart = routine.trajectory("LeftStartToRightStart");
 
-      routine.active().onTrue(
-          Commands.sequence(
-              Commands.runOnce(() -> {
-                  leftStartToRightStart.getInitialPose().ifPresent(pose -> m_logger.log("InitialPose", pose));
-                  m_logger.log("ActiveTrajectory", "LeftStartToRightStart");
-              }),
-              leftStartToRightStart.resetOdometry(),
-              leftStartToRightStart.cmd()));
-      return routine;
-  }
-
-  public AutoRoutine rightStartToLeftStart() {
-      AutoRoutine routine = m_autoFactory.newRoutine("RightStartToLeftStart");
-      AutoTrajectory rightStartToLeftStart = routine.trajectory("RightStartToLeftStart");
-
-      routine.active().onTrue(
-          Commands.sequence(
-              Commands.runOnce(() -> {
-                  rightStartToLeftStart.getInitialPose().ifPresent(pose -> m_logger.log("InitialPose", pose));
-                  m_logger.log("ActiveTrajectory", "RightStartToLeftStart");
-              }),
-              rightStartToLeftStart.resetOdometry(),
-              rightStartToLeftStart.cmd()));
-      return routine;
-  }
+  
 }
