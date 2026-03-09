5fa45c8e7b90ed10d99fe969b86a44c89a1d751a
ElochiFighter <elochi.walker@outlook.com>
2026-03-09 14:04:14 -0700

    will-have-eradicated-yams-from-feeder

--- src/main/java/frc/robot/subsystems/feeder/Feeder.java
+++ src/main/java/frc/robot/subsystems/feeder/Feeder.java
@@ -1,44 +1,31 @@
 package frc.robot.subsystems.feeder;
-
-import static edu.wpi.first.units.Units.Amps;
 import static edu.wpi.first.units.Units.Inches;
 import static edu.wpi.first.units.Units.Milliseconds;
-import static edu.wpi.first.units.Units.Pounds;
-import static edu.wpi.first.units.Units.RPM;
 import static edu.wpi.first.units.Units.Seconds;
 
-import java.util.function.Supplier;
-
 import com.ctre.phoenix6.configs.CANrangeConfiguration;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.CANrange;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.epilogue.Logged.Strategy;
 import edu.wpi.first.math.filter.LinearFilter;
-import edu.wpi.first.math.system.plant.DCMotor;
-import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj.Alert;
 import edu.wpi.first.wpilibj.Alert.AlertType;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.lib.Logger;
+import frc.lib.PhoenixUtil;
 import frc.lib.Watchdawg;
 import frc.robot.constants.Ports;
-import yams.mechanisms.config.FlyWheelConfig;
-import yams.mechanisms.velocity.FlyWheel;
-import yams.motorcontrollers.SmartMotorController;
-import yams.motorcontrollers.SmartMotorControllerConfig;
-import yams.motorcontrollers.SmartMotorControllerConfig.ControlMode;
-import yams.motorcontrollers.SmartMotorControllerConfig.MotorMode;
-import yams.motorcontrollers.SmartMotorControllerConfig.TelemetryVerbosity;
-import yams.motorcontrollers.remote.TalonFXWrapper;
 
 @Logged(strategy = Strategy.OPT_IN)
 public class Feeder extends SubsystemBase {
-  private final FlyWheel m_mechanism;
   private final CANrange m_fuelSensor;
   private final LinearFilter m_fuelSensorFilter;
   private final Logger m_log;
@@ -49,26 +36,6 @@ public class Feeder extends SubsystemBase {
 
   public Feeder() {
     m_motor = new TalonFX(Ports.kFeederBelt.canId(), Ports.kFeederBelt.canbus());
-
-    SmartMotorControllerConfig motorControllerConfig = new SmartMotorControllerConfig(this)
-        .withControlMode(ControlMode.OPEN_LOOP)
-        .withIdleMode(MotorMode.COAST)
-        // .withClosedLoopController(0.3, 0, 0.01)
-        // .withFeedforward(new SimpleMotorFeedforward(0.05, 0.12, 0))
-        .withGearing(2)
-        .withStatorCurrentLimit(Amps.of(40))
-        .withTelemetry("FeederMotor", TelemetryVerbosity.LOW);
-
-    SmartMotorController motorController = new TalonFXWrapper(m_motor, DCMotor.getKrakenX44(1), motorControllerConfig);
-    FlyWheelConfig beltConfig = new FlyWheelConfig(motorController)
-        .withMass(Pounds.of(0.5))
-        .withUpperSoftLimit(RPM.of(6000))
-        .withLowerSoftLimit(RPM.of(-6000))
-        .withDiameter(Inches.of(2.0))
-        .withTelemetry("Feeder", TelemetryVerbosity.LOW);
-
-    m_mechanism = new FlyWheel(beltConfig);
-
     m_fuelSensor = new CANrange(Ports.kFeederFuelSensor.canId(), Ports.kFeederFuelSensor.canbus());
     CANrangeConfiguration fuelSensorConfig = new CANrangeConfiguration();
     m_fuelSensor.getConfigurator().apply(fuelSensorConfig);
@@ -76,6 +43,20 @@ public class Feeder extends SubsystemBase {
     m_fuelSensorFilter = LinearFilter.movingAverage(5);
     m_log = new Logger(getClass());
     m_watchdog = new Watchdawg(getClass());
+
+    configureMotor();
+  }
+
+  private void configureMotor() {
+    TalonFXConfiguration config = new TalonFXConfiguration();
+    config.MotorOutput
+      .withNeutralMode(NeutralModeValue.Coast);
+    config.CurrentLimits
+      .withStatorCurrentLimitEnable(true)
+      .withStatorCurrentLimit(90);
+
+    PhoenixUtil.tryUntilOk(5, () -> m_motor.getConfigurator().apply(config));
+    
   }
 
   private boolean getFuelSensorTripped() {
@@ -94,10 +75,6 @@ public class Feeder extends SubsystemBase {
     m_log.log("sensorTripped", getFuelSensorTripped());
 
     m_watchdog.start();
-    m_mechanism.updateTelemetry();
-    m_watchdog.end("updateTelemetry");
-
-    m_watchdog.start();
     m_talonConnectionAlert.set(!m_motor.isAlive());
     boolean highCurrent = m_motor.getStatorCurrent().getValueAsDouble() > 30;
     boolean notMoving = Math.abs(m_motor.getVelocity().getValueAsDouble()) < 2;
@@ -105,21 +82,8 @@ public class Feeder extends SubsystemBase {
     m_watchdog.end("updateAlerts");
   }
 
-  @Override
-  public void simulationPeriodic() {
-    m_mechanism.simIterate();
-  }
-
-  public Command setSpeedCommand(AngularVelocity angularVelocity) {
-    return m_mechanism.setSpeed(angularVelocity);
-  }
-
-  public Command setVoltageCommand(Supplier<Voltage> voltage) {
-    return m_mechanism.setVoltage(voltage);
-  }
-
-  public Command setVoltageCommand(Voltage voltage) {
-    return m_mechanism.setVoltage(voltage);
+  public Command setVoltageCommand(Voltage volts) {
+    return Commands.runOnce(() -> m_motor.setVoltage(volts.baseUnitMagnitude()));
   }
 
 }
