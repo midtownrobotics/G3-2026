2bbd1eb2c8bc8175f2cd54c70fb00f4ff2d99a86
Gray486 <me@grayjn.com>
2026-03-27 16:21:42 -0400

    Update at 'Fri Mar 27 16:21:42 EDT 2026'

--- src/main/java/frc/robot/constants/Constants.java
+++ src/main/java/frc/robot/constants/Constants.java
@@ -23,7 +23,7 @@ public class Constants {
   public static final double kLinearSpeedMultiplier = 1;
   public static final double kAngluarSpeedMultiplier = 0.9;
 
-  public static final LinearVelocity kMaxLinearSpeed = MetersPerSecond.of(3);
+  public static final LinearVelocity kMaxLinearSpeed = MetersPerSecond.of(4);
 
   /** Calculated based on tuner constants. */
   public static final AngularVelocity kAngularMaxSpeed = RadiansPerSecond.of(
