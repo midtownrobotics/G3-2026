751ccd372ac87ab74e5a575092e7fe88bbfb6ca6
Gray486 <me@grayjn.com>
2026-03-28 14:05:16 -0400

    Update at 'Sat Mar 28 14:05:16 EDT 2026'

--- src/main/java/frc/robot/subsystems/intake/IntakePivotIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/intake/IntakePivotIOTalonFX.java
@@ -4,6 +4,7 @@ import static edu.wpi.first.units.Units.Amps;
 import static edu.wpi.first.units.Units.Degrees;
 import static edu.wpi.first.units.Units.DegreesPerSecond;
 import static edu.wpi.first.units.Units.DegreesPerSecondPerSecond;
+import static edu.wpi.first.units.Units.Rotations;
 
 import com.ctre.phoenix6.BaseStatusSignal;
 import com.ctre.phoenix6.StatusSignal;
@@ -57,10 +58,10 @@ public class IntakePivotIOTalonFX implements IntakePivotIO {
     // Configure CANcoder
     CANcoderConfiguration canCoderConfig = new CANcoderConfiguration();
 
-    // canCoderConfig.MagnetSensor = new MagnetSensorConfigs()
-    //     .withSensorDirection(SensorDirectionValue.CounterClockwise_Positive)
-    //     .withAbsoluteSensorDiscontinuityPoint(Degrees.of(350));
-    //     // .withMagnetOffset(Degrees.of(8.1 * kSensorToMechanismRatio));
+    canCoderConfig.MagnetSensor = new MagnetSensorConfigs()
+        .withSensorDirection(SensorDirectionValue.CounterClockwise_Positive)
+        .withAbsoluteSensorDiscontinuityPoint(Degrees.of(350))
+        .withMagnetOffset(Rotations.of(0.123779296875));
 
     PhoenixUtil.tryUntilOk(5, () -> m_encoder.getConfigurator().apply(canCoderConfig));
 
