d9b38ad73eb423290edc6ff0163967e8387b5597
Gray486 <me@grayjn.com>
2026-04-09 19:11:24 -0400

    Update at 'Thu Apr 09 19:11:24 EDT 2026'

--- src/main/java/frc/robot/subsystems/indexer/IndexerIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/indexer/IndexerIOTalonFX.java
@@ -51,7 +51,9 @@ public class IndexerIOTalonFX implements InexerIO {
 
     config.CurrentLimits = new CurrentLimitsConfigs()
         .withStatorCurrentLimitEnable(true)
-        .withStatorCurrentLimit(Amps.of(90));
+        .withStatorCurrentLimit(Amps.of(90))
+        .withSupplyCurrentLimitEnable(true)
+        .withSupplyCurrentLimit(Amps.of(50));
 
     m_motor.getConfigurator().apply(config);
 
--- src/main/java/frc/robot/subsystems/shooter/TurretIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/shooter/TurretIOTalonFX.java
@@ -84,7 +84,9 @@ public class TurretIOTalonFX implements TurretIO {
 
     config.CurrentLimits = new CurrentLimitsConfigs()
         .withStatorCurrentLimitEnable(true)
-        .withStatorCurrentLimit(Amps.of(90));
+        .withStatorCurrentLimit(Amps.of(90))
+        .withSupplyCurrentLimitEnable(true)
+        .withSupplyCurrentLimit(Amps.of(60));
 
     config.OpenLoopRamps = new OpenLoopRampsConfigs().withVoltageOpenLoopRampPeriod(Seconds.of(0.25));
 
