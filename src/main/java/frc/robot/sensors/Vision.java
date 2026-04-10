595259b2c1b99183b541ef119482dc0bbac57d58
Gray486 <me@grayjn.com>
2026-04-10 12:50:16 -0400

    Update at 'Fri Apr 10 12:50:16 EDT 2026'

--- src/main/java/frc/robot/sensors/Vision.java
+++ src/main/java/frc/robot/sensors/Vision.java
@@ -56,7 +56,7 @@ public class Vision extends SubsystemBase {
     if (Robot.isSimulation()) {
       m_visionSim = new VisionSystemSim("main");
       m_visionSim.addAprilTags(AprilTagFieldLayout.loadField(AprilTagFields.k2026RebuiltWelded));
-      m_cameras.forEach(c -> m_visionSim.addCamera(c.getSimCamera(), c.getRobotToCamera()));
+      // m_cameras.forEach(c -> m_visionSim.addCamera(c.getSimCamera(), c.getRobotToCamera()));
     }
 
     m_observations = TimeInterpolatableBuffer.createBuffer(0.1);
