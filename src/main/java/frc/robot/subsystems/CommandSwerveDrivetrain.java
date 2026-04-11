8b7bbafc9364e6a0691b8dab668fbd5084595ae9
Gray486 <me@grayjn.com>
2026-04-11 10:31:45 -0400

    Update at 'Sat Apr 11 10:31:45 EDT 2026'

--- src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
@@ -63,8 +63,8 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
   /** Swerve request to apply during field-centric path following */
   private final SwerveRequest.ApplyFieldSpeeds m_pathApplyFieldSpeeds = new SwerveRequest.ApplyFieldSpeeds();
 
-  private final PIDController m_pathXController = new PIDController(7.5, 0, 0);
-  private final PIDController m_pathYController = new PIDController(7.5, 0, 0);
+  private final PIDController m_pathXController = new PIDController(8.8, 0, 0);
+  private final PIDController m_pathYController = new PIDController(8.8, 0, 0);
   private final PIDController m_pathThetaController = new PIDController(7, 0, 0);
 
   /* Swerve requests to apply during SysId characterization */
