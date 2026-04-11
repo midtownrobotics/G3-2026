1c028184af6b75992b5e9a1e44989fd623e72be4
jrrachat <john.rachwalski1@gmail.com>
2026-04-11 09:42:02 -0400

    feed autos

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -163,13 +163,13 @@ public class Robot extends LoggedRobot {
         new Transform3d(
             new Translation3d(Inches.of(-8.758), Inches.of(-14.541), Inches.of(8.022)),
             new Rotation3d(Degrees.zero(), Degrees.of(-15), Degrees.of(-33.26 - 90))),
-            () -> (DriverStation.isAutonomous() || !turretCamera.isConnected()));
+        () -> (DriverStation.isAutonomous() || !turretCamera.isConnected()));
     Camera rearLeft = new Camera(
         "Rear Left",
         new Transform3d(
             new Translation3d(Inches.of(-7.692), Inches.of(14.396), Inches.of(14.217)),
-            new Rotation3d(Degrees.zero(), Degrees.of(-10), Degrees.of(31.475 + 90))),  
-            () -> (DriverStation.isAutonomous() || !turretCamera.isConnected()));
+            new Rotation3d(Degrees.zero(), Degrees.of(-10), Degrees.of(31.475 + 90))),
+        () -> (DriverStation.isAutonomous() || !turretCamera.isConnected()));
     Camera frontLeft = new Camera(
         "Front Left",
         new Transform3d(
@@ -284,6 +284,9 @@ public class Robot extends LoggedRobot {
     m_autoChooser.addRoutine("DCMP LeftDoubleSwipe + Depot", m_autoRoutines::LeftDoubleSwipeDepot);
     m_autoChooser.addRoutine("DCMP RightDoubleSwipe", m_autoRoutines::RightDoubleSwipe);
     m_autoChooser.addRoutine("DCMP LeftDoubleSwipe", m_autoRoutines::LeftDoubleSwipe);
+    m_autoChooser.addRoutine("Left Feeding + Depot", m_autoRoutines::LeftStartFeedingDepot);
+    m_autoChooser.addRoutine("Left Feeding", m_autoRoutines::LeftStartFeeding);
+    m_autoChooser.addRoutine("Right Feeding", m_autoRoutines::RightStartFeeding);
 
     SmartDashboard.putData("Auto Chooser", m_autoChooser);
     RobotModeTriggers.autonomous().whileTrue(m_autoChooser.selectedCommandScheduler());
