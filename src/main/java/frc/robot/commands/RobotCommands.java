24321b721bab92d91bff035284a7758766d7e54b
Arun <apalaniappan@anduril.com>
2026-04-23 19:47:25 -0400

    try this

--- src/main/java/frc/robot/commands/RobotCommands.java
+++ src/main/java/frc/robot/commands/RobotCommands.java
@@ -1,7 +1,16 @@
 package frc.robot.commands;
 
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.RPM;
+import static edu.wpi.first.units.Units.Seconds;
+import static edu.wpi.first.units.Units.Volts;
+
 import java.util.function.Supplier;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Command.InterruptionBehavior;
+import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.RobotState;
 import frc.robot.RobotState.ShooterState;
 import frc.robot.constants.Constants;
