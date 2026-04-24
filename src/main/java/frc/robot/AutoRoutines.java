372356be77554329860b4bb2d835ea7f8fcf0766
Arun <apalaniappan@anduril.com>
2026-04-23 20:08:31 -0400

    fixing build issues

--- src/main/java/frc/robot/AutoRoutines.java
+++ src/main/java/frc/robot/AutoRoutines.java
@@ -1,5 +1,9 @@
 package frc.robot;
 
+import choreo.auto.AutoFactory;
+import choreo.auto.AutoRoutine;
+import choreo.auto.AutoTrajectory;
+import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.commands.RobotCommands;
 
 public class AutoRoutines {
