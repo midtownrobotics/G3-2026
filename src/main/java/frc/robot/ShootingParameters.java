5f48b75d7ffd1f4a310daf264e9815656b59398d
Gray486 <me@grayjn.com>
2026-05-01 16:16:56 -0500

    Update at 'Fri May 01 16:16:56 CDT 2026'

--- src/main/java/frc/robot/ShootingParameters.java
+++ src/main/java/frc/robot/ShootingParameters.java
@@ -98,13 +98,13 @@ public class ShootingParameters {
       Map.entry(Feet.of(8).in(Meters), RPM.of(1600+50+25+50).in(RadiansPerSecond)),
       Map.entry(Feet.of(9).in(Meters), RPM.of(1700+50+25+50).in(RadiansPerSecond)),
       Map.entry(Feet.of(10).in(Meters), RPM.of(1700+50+25+50).in(RadiansPerSecond)),
-      Map.entry(Feet.of(11).in(Meters), RPM.of(1800+50+25+50).in(RadiansPerSecond)),
-      Map.entry(Feet.of(12).in(Meters), RPM.of(1850+50+25+50).in(RadiansPerSecond)),
-      Map.entry(Feet.of(13).in(Meters), RPM.of(2000+50+25+50).in(RadiansPerSecond)),
-      Map.entry(Feet.of(14).in(Meters), RPM.of(2050+50+25+50).in(RadiansPerSecond)),
-      Map.entry(Feet.of(15).in(Meters), RPM.of(2100+50+25+50).in(RadiansPerSecond)),
-      Map.entry(Feet.of(16).in(Meters), RPM.of(2200+50+25+50).in(RadiansPerSecond)),
-      Map.entry(Feet.of(25).in(Meters), RPM.of(2600+50+25+50).in(RadiansPerSecond)) //
+      Map.entry(Feet.of(11).in(Meters), RPM.of(1800+50+50).in(RadiansPerSecond)),
+      Map.entry(Feet.of(12).in(Meters), RPM.of(1850+25+50).in(RadiansPerSecond)),
+      Map.entry(Feet.of(13).in(Meters), RPM.of(2000+25+50).in(RadiansPerSecond)),
+      Map.entry(Feet.of(14).in(Meters), RPM.of(2050+25+50).in(RadiansPerSecond)),
+      Map.entry(Feet.of(15).in(Meters), RPM.of(2100+25+50).in(RadiansPerSecond)),
+      Map.entry(Feet.of(16).in(Meters), RPM.of(2200+25+50).in(RadiansPerSecond)),
+      Map.entry(Feet.of(25).in(Meters), RPM.of(2600+25+50).in(RadiansPerSecond)) //
   );
 
   private final InterpolatingDoubleTreeMap m_feedingFlywheelVelocityMap = InterpolatingDoubleTreeMap.ofEntries(
